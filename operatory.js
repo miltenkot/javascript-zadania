0a2b7eec8deb92b02f23863938807968527cff75
miltenkot <miltenkot@gmail.com>
2021-01-10 15:57:12 +0100

    3. Operatory
    Zadanie 1
    
    W konsoli stwórz dwie zmienne i przypisz do nich dwie wartości boolean. Porównaj je za pomocą odpowiedniego operatora.
    
    Zadanie 2
    
    W konsoli stwórz trzy zmienne. Dwie niech przechowują dowolne liczby, a trzecia o nazwie moduloResult niech przechowuje liczbę 0. Oblicz resztę z dzielenia (modulo) tych liczb i zapisz wynik w zmiennej moduloResult. Wypisz zmienną moduloResult w konsoli.
    
    Zadanie 3
    
    W konsoli stwórz trzy zmienne. Dwie niech przechowują dowolne stringi, a jedna o nazwie stringsResult, niech przechowuje pusty string np. let stringsResult = "" Połącz stringi za pomocą konkatenacji i zapisz wynik w zmiennej stringsResult oraz wypisz go w konsoli.
    
    Zadanie 4
    
    W konsoli stwórz dwie zmienne:
    
    var someNumber = 425;
    var someString = "425";
    Porównaj te zmienne za pomocą operatorów == oraz ===. Wypisz wyniki w konsoli. Opowiedz grupie o wynikach
    
    Zadanie 5
    
    W konsoli stwórz zmienną o nazwie counter. Wstaw do niej liczbę 30. Wypisz jej wartość w konsoli, a następnie:
    
    za pomocą inkrementacji zwiększ wartość zmiennej counter
    wypisz ją w konsoli
    za pomocą dekrementacji zmniejsz wartość zmiennej counter
    wypisz ją w konsoli.
    Zadanie 6
    
    W konsoli stwórz trzy zmienne. Dwie niech przechowują dowolne liczby, a jedna o nazwie result, niech przechowuje null. Sprawdź czy liczba pierwsza jest większa od drugiej za pomocą odpowiedniego operatora i zapisz wynik w zmiennej result. Wypisz tą zmienną w konsoli.

---
+++ operatory.js
@@ -0,0 +1,46 @@
+// Zadanie 1
+let one = true;
+let two = true;
+
+console.log(one===two);
+
+// Zadanie 2
+let num1 = 15;
+let num2 = 10;
+let moduloResult = 0;
+
+moduloResult = num1 % num2;
+console.log(moduloResult);
+
+// Zadanie 3
+let str1 = 'Ala ma';
+let str2 = ' kredyt.';
+let stringsResult = "";
+
+stringsResult = str1.concat(str2);
+console.log(stringsResult);
+
+// Zadanie 4
+var someNumber = 425;
+var someString = "425";
+
+console.log(someNumber==someString);  //operandy są równie.
+console.log(someNumber===someString); //operandy są równie lecz nie są tego samego typu.
+
+// Zadanie 5
+let counter = 30;
+console.log(counter);
+
+counter++;
+console.log(counter);
+
+counter--;
+console.log(counter);
+
+// Zadanie 6
+let num1 = 20;
+let num2 = 35;
+let result = null;
+
+result = (num1 > num2);
+console.log(result);
