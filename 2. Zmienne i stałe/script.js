4e9b287723413df5bd802c2bb0393ffb14c95d67
miltenkot <miltenkot@gmail.com>
2021-02-05 19:14:19 +0100

    Add Zmienne i stałe

---
+++ 2. Zmienne i stałe/script.js
@@ -0,0 +1,24 @@
+// Zadanie 1
+let nr = 23;
+let str = 'dwadzieścia trzy';
+let bool = true;
+let special= 0/0;
+
+console.log(nr);
+console.log(str);
+console.log(nr + str);
+console.log(bool);
+console.log(special);
+
+// Zadanie 2
+let num1 = 1;
+let num2 = 2;
+let sum = 0;
+
+sum = num1 + num2;
+console.log(sum);
+
+// Zadanie 3
+let nothing;
+console.log(nothing);
+//undefined- zmienna została zadeklarowana ale nie ma przypisanej wartości.
